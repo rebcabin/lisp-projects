2ba24b4b2e7b1a7d47fb349ec1d7d1c861d4b953
rebcabin <bc.beckman@gmail.com>
2016-02-20 09:58:19 -0800

    begin documentation

--- ncurses/world.lisp
+++ ncurses/world.lisp
@@ -1,3 +1,12 @@
+;;; The world is be a sequence of storeys. Each storey is a matrix of cells or
+;;; tiles. Tiles contain things. Every tile contains one thing of type "geo."
+;;; The default geo is nil, which means "nothing but the floor." Other geos are
+;;; walls, doors, rocks (e.g., granite, quartz). A tile may contain one or zero
+;;; "critters." A critter is either "me" or a "monster." A tile may contain zero
+;;; or more "items." Some items include "treasures" and "traps." A tile may be
+;;; lit or dark
+
+
 (defconstant +wall+  1)
 (defconstant +floor+ 2)
 (defconstant +me+    3)
