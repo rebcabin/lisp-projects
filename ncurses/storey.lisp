ae90f9195c3483a369eb21b0b371eb0378f2b719
rebcabin <bc.beckman@gmail.com>
2016-02-20 17:12:43 -0800

    abstract storey  to destruct

--- ncurses/storey.lisp
+++ ncurses/storey.lisp
@@ -1,12 +1,14 @@
-;;; A storey is a matrix of /tiles/.
+;;; A storey has a matrix of /tiles/.
 
 (defconstant +storey-width+  256)
 (defconstant +storey-height+ 256)
 
-(defun make-storey (&key (width +storey-width+)
-                      (height +storey-height+))
+(defun make-storey-matrix (&key
+                             (width +storey-width+)
+                             (height +storey-height+))
   (make-array `(,height ,width)
               :initial-element nil))
 
-
+(defstruct storey
+  (matrix))
 
--- ncurses/test/storey.lisp
+++ ncurses/test/storey.lisp
@@ -3,8 +3,17 @@
 
 (quickcheck
  (is= 4 4)
- (let ((m (make-storey :width 10 :height 6)))
+ (let ((m (make-storey-matrix :width 10 :height 6)))
    (setf (aref m 2 1)
          (make-instance 'tile))
-   (is equal (type-of (aref m 2 1) 'tile)))
-)
+   (is equal (type-of (aref m 2 1)) 'tile))
+ (let* ((s (make-storey :matrix
+                        (make-storey-matrix :width 10 :height 6)))
+        (t0 (type-of (storey-matrix s)))
+        (cl (first t0))
+        (et (second t0))
+        (ds (third t0)))
+   (is equal cl 'simple-array)
+   (is equal et t)
+   (is equal ds '(6 10)))
+ )
