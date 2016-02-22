7e7250240aafa29b369c33fb729cfdb9369310e8
rebcabin <bc.beckman@gmail.com>
2016-02-21 21:56:49 -0800

    increment to testing put-room

--- ncurses/test/room.lisp
+++ ncurses/test/room.lisp
@@ -31,11 +31,15 @@
                            :top 2
                            :width (- (box-width bb) 2)
                            :height (- (box-height bb) 3)))
-        (rm (create-m-room rb s)))
+        (rm (create-m-room rb s))
+        (m  (storey-matrix s))
+        )
    (describe rb)
    (describe s)
-   (put-room rm)
-   (print (storey-matrix s))
+   ;; (put-room rm)
+   (setf (aref m 1 2) #\*)
+   (let ((*print-length* 1000))
+     (print m))
    )
  )
 
