a770df2ff944d68b0a222b0c016ec5e088ca6919
rebcabin <bc.beckman@gmail.com>
2016-02-21 21:09:48 -0800

    initialize walls with +regular-wall-glyph+

--- ncurses/geo.lisp
+++ ncurses/geo.lisp
@@ -1,9 +1,14 @@
-
-
 (defclass geo ()
   ((render-char :initform +regular-floor-glyph+ :initarg :render-char)))
 
 (defclass wall (geo) ())
+(defmethod initialize-instance
+    :after
+    ((w wall)
+     &key (render-char +regular-wall-glyph+))
+  (setf (slot-value w 'render-char)
+        render-char))
+
 (defclass door (geo) ())
 (defclass rock (geo) ())
 (defclass trap (geo) ())
--- ncurses/test/geo.lisp
+++ ncurses/test/geo.lisp
@@ -3,5 +3,10 @@
 (quickcheck
  (is= 4 4)
 
+ (let ((w (make-instance 'wall)))
+   (is equal
+       (slot-value w 'render-char)
+       +regular-wall-glyph+))
+
  )
 
